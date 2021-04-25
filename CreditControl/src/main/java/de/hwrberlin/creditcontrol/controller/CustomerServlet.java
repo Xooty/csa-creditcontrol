e0ec5264bb75928db5849a5472139a90c5315c64
Lockheed <Lockheed@144HzMaschine>
2021-04-25 15:00:37 +0200

    CustomerServlet

---
+++ CreditControl/src/main/java/de/hwrberlin/creditcontrol/controller/CustomerServlet.java
@@ -0,0 +1,12 @@
+package de.hwrberlin.creditcontrol.controller;
+
+import javax.servlet.*;
+
+
+public class CustomerServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {  
+        response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+}
